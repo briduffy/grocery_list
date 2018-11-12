4719e8e1bf82cc0b2e09dd00a8a55af53f8a5bf2
Brianne Duffy <duffymbrianne@gmail.com>
2018-11-12 09:57:41 -0700

    same error

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,54 @@
-import React, { Component } from 'react'
+import React from 'react'
 import ItemForm from './components/ItemForm'
 import GroceryItemList from './components/GroceryItemList'
 
-class App extends Component {
+class App extends React.Component {
   state = { items: [] }
 
   componentDidMount() {
     fetch('/api/items')
-      .then( res => res.json() )
-      .then( items => this.setState ({ items }))
+      .then(res => res.json())
+      .then( items => this.setState ({ items: items }))
   }
 
   addItem = (name, type) => {
-    const { items } = this.state
-    const id = Math.floor(( 1 + Math.random()) * 0x1000).toString()
-    this.setState({ items: [...items, { id, name, type }] })
+    const item = { name }
+    fetch('/api/items', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(item)
+    }).then(res => res.json())
+      .then(item => {
+        const { items } = this.state;
+        this.setState({ items: [...items, item] })
+      })
   }
 
   updateItem = (id) => {
-    let items = this.state.items.map( i => {
-      if ( i.id === id )
-        return {...i, purchased: !i.purchased }
-      return i
+    fetch(`/api/items/${id}`, { method: 'PUT' })
+      .then(res => res.json())
+      .then(item => {
+        const items = this.state.items.map( i => {
+          if ( i.id === id )
+            return {item}
+          return i
+       })
+    this.setState({ items: items })
     })
-    this.setState({ items })
   }
 
   deleteItem = (id) => {
-    const { items } = this.state
-    this.setState({ items: items.filter( i => i.id !== id )})
-  }
+    fetch(`/api/items/${id}`, { method: 'DELETE' })
+      .then( () => {
+        const { items } = this.state;
+        this.setState({ items: items.filter( i => i.id !== id ) })
+      })
+   }
+
+
 
   render() {
     return (
